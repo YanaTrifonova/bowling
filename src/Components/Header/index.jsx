0cc2f2514925a920f21dc2d23b496cdb5b6093c0
YanaTrifonova <trifonova.yana.prof@gmail.com>
2021-09-18 16:57:48 +0200

    init commit for Header Component

---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+export default function Header() {
+  return (
+    <Typography variant="h1" component="div" gutterBottom>🎳 Bowling App</Typography>
+  );
+}
