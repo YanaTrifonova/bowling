d35d9e2a50ba5ca4503aa3bb0b53dcfe4a2071c5
YanaTrifonova <trifonova.yana.prof@gmail.com>
2021-09-20 23:22:21 +0200

    code refactoring separate code within methods

--- src/store/GameState/reducer.js
+++ src/store/GameState/reducer.js
@@ -1,37 +1,82 @@
 import {CREATE_NEW_GAME, DELETE_GAME, RESET_ALL_GAMES, UPDATE_SCORES} from "./actions";
 
-const initialState = {
+export const initialState = {
   games: []
 }
 
+function makeCopyOfState(playerToUpdate, state, action) {
+  const gameIndexToUpdate = action.payload.gameIndex - 1;
+  const gameToUpdate = state.games[gameIndexToUpdate];
+  const gameCopy = Object.keys(gameToUpdate).reduce((game, player) => ({
+    ...game,
+    [player]: gameToUpdate[player]
+  }), {});
+  const playerCopy = JSON.parse(JSON.stringify(gameCopy[playerToUpdate]));
+  const frameToUpdate = playerCopy[action.payload.frameIndex];
+  return {
+    gameIndexToUpdate,
+    gameCopy,
+    playerCopy,
+    frameToUpdate
+  };
+}
+
+export function updateCurrentFrame(frameToUpdate, resultScore, actionScore) {
+  frameToUpdate.scores += resultScore;
+
+  if (actionScore === 10) {
+    frameToUpdate.isStrike = true;
+  }
+
+  if (frameToUpdate.scores === 10 && !frameToUpdate.isStrike) {
+    frameToUpdate.isSpare = true;
+  }
+}
+
+export function enableNextFrame(frameToEnable, kickIndex, isStrike) {
+  const secondKick = kickIndex === 1;
+  if (frameToEnable && (secondKick || isStrike)) {
+    frameToEnable.isDisabled = false;
+  }
+}
+
+export function disablePreviousFrame(frameToDisable) {
+  if (frameToDisable) {
+    frameToDisable.isDisabled = true;
+  }
+}
+
+export function updatePreviousTotalScores(previousFrame, previousPreviousFrame, kickIndex, resultScore) {
+  const isPreviousSpare = previousFrame ? previousFrame.isSpare : false;
+  const isPreviousStrike = previousFrame ? previousFrame.isStrike : false;
+  const isPreviousPreviousStrike = previousPreviousFrame ? previousPreviousFrame.isStrike : false;
+  const isFirstKick = kickIndex === 0;
+  const isLastFrameKick = kickIndex === 2; // 10th frame, third kick
+
+  if (previousFrame && isPreviousSpare && isFirstKick) {
+    previousFrame.totalScores += resultScore;
+  }
+  if (previousFrame && isPreviousStrike && !isLastFrameKick) {
+    previousFrame.totalScores += resultScore;
+  }
+  if (previousPreviousFrame && isPreviousStrike && isPreviousPreviousStrike && isFirstKick) {
+    previousPreviousFrame.totalScores += resultScore;
+    previousFrame.totalScores += resultScore;
+  }
+}
+
+function createNewState(gameCopy, playerToUpdate, playerCopy, gameIndexToUpdate, state) {
+  gameCopy[playerToUpdate] = playerCopy;
+  const gamesCopy = [...state.games];
+  gamesCopy[gameIndexToUpdate] = gameCopy
+
+  return {
+    ...state,
+    games: gamesCopy
+  };
+}
+
 export default function (state = initialState, action) {
-  function createNewState(gameCopy, playerToUpdate, playerCopy, gameIndexToUpdate) {
-    gameCopy[playerToUpdate] = playerCopy;
-    const gamesCopy = [...state.games];
-    gamesCopy[gameIndexToUpdate] = gameCopy
-    return {
-      ...state,
-      games: gamesCopy
-    };
-  }
-
-  function makeCopyOfState(playerToUpdate) {
-    const gameIndexToUpdate = action.payload.gameIndex - 1;
-    const gameToUpdate = state.games[gameIndexToUpdate];
-    const gameCopy = Object.keys(gameToUpdate).reduce((game, player) => ({
-      ...game,
-      [player]: gameToUpdate[player]
-    }), {});
-    const playerCopy = JSON.parse(JSON.stringify(gameCopy[playerToUpdate]));
-    const frameToUpdate = playerCopy[action.payload.frameIndex];
-    return {
-      gameIndexToUpdate,
-      gameCopy,
-      playerCopy,
-      frameToUpdate
-    };
-  }
-
   switch (action.type) {
     case CREATE_NEW_GAME : {
       const game = action.payload.reduce((a, v) => {
@@ -79,44 +124,19 @@ export default function (state = initialState, action) {
         gameCopy,
         playerCopy,
         frameToUpdate
-      } = makeCopyOfState(playerToUpdate);
+      } = makeCopyOfState(playerToUpdate, state, action);
 
-      if (kickIndex === 1 && frameIndexToUpdate !== playerCopy.length || action.payload.score === 10) {
-        if (playerCopy[frameIndexToUpdate + 1]) {
-          playerCopy[frameIndexToUpdate + 1].isDisabled = false;
-        }
-      }
-      if (frameIndexToUpdate > 0) {
-        playerCopy[frameIndexToUpdate - 1].isDisabled = true;
-      }
+      updateCurrentFrame(frameToUpdate, resultScore, action.payload.score);
 
+      enableNextFrame(playerCopy[frameIndexToUpdate + 1], kickIndex,  frameToUpdate.isStrike);
+      disablePreviousFrame(playerCopy[frameIndexToUpdate - 1]);
 
-      frameToUpdate.scores += resultScore;
-
-      if (action.payload.score === 10) {
-        frameToUpdate.isStrike = true;
-      }
-      if (frameToUpdate.scores === 10 && frameToUpdate.isStrike === false) {
-        frameToUpdate.isSpare = true;
-      }
-
-      if (frameIndexToUpdate !== 0 && playerCopy[frameIndexToUpdate - 1].isSpare && kickIndex === 0) {
-        playerCopy[frameIndexToUpdate - 1].totalScores += resultScore;
-      }
-      if (frameIndexToUpdate !== 0 && playerCopy[frameIndexToUpdate - 1].isStrike && kickIndex !== 2) {
-        playerCopy[frameIndexToUpdate - 1].totalScores += resultScore;
-      }
-      if (frameIndexToUpdate >= 2 && playerCopy[frameIndexToUpdate - 1].isStrike && playerCopy[frameIndexToUpdate
-                                                                                               - 2].isStrike
-          && kickIndex === 0) {
-        playerCopy[frameIndexToUpdate - 2].totalScores += resultScore;
-        playerCopy[frameIndexToUpdate - 1].totalScores += resultScore;
-      }
+      updatePreviousTotalScores(playerCopy[frameIndexToUpdate - 1], playerCopy[frameIndexToUpdate - 2], kickIndex, resultScore);
 
       frameToUpdate.totalScores =
         frameToUpdate.scores + (frameIndexToUpdate === 0 ? 0 : playerCopy[frameIndexToUpdate - 1].totalScores);
 
-      return createNewState(gameCopy, playerToUpdate, playerCopy, gameIndexToUpdate);
+      return createNewState(gameCopy, playerToUpdate, playerCopy, gameIndexToUpdate, state);
     }
 
     default:
