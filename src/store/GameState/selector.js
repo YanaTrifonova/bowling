ff6e7fc4838d776b2dfeee2ec9d36f8a3267d8d8
YanaTrifonova <trifonova.yana.prof@gmail.com>
2021-09-20 19:12:49 +0200

    removed commented code, styles

--- src/store/GameState/selector.js
+++ src/store/GameState/selector.js
@@ -1,13 +1,6 @@
 export const getAllGames = state => state.games;
-export const getFrameScore = (gameIndex, playerName, frameIndex) => state => state.games[gameIndex - 1][playerName][frameIndex]?.totalScores;
-// export const getTotalScoreAcrossGames = (playerName) => state => state.games.map((playerGameTable, ind, arr) => playerGameTable[playerName][playerGameTable[playerName].length - 1].totalScores).reduce((acc, val) => (acc + val), 0);
-// export const getWonGames = (playerName) => state => state.games
-//                                                          .map((playerGameTable, ind, arr) =>
-//                                                            Object.keys(playerGameTable) // player names
-//                                                                  .reduce((acc, val) => ([...acc, [val, playerGameTable[val][playerGameTable[val].length - 1].totalScores]]), []))
-//                                                          .map((finalScores, ind, arr) =>
-//                                                            finalScores.sort((l, r) => r[1] - l[1])[0][0] === playerName)
-//                                                          .reduce((acc, val) => acc + val, 0);
+export const getFrameScore = (gameIndex, playerName, frameIndex) => state =>
+  state.games[gameIndex - 1][playerName][frameIndex]?.totalScores;
 
 export const getTotalScoreAcrossGames = (players) => state => {
   const scores = players.reduce((a, v) => ({ ...a, [v]: 0}), {});
@@ -25,7 +18,8 @@ export const getTotalScoreAcrossGames = (players) => state => {
 export const getWonGames = (players) => state => {
   const scores = players.reduce((a, v) => ({ ...a, [v]: 0}), {});
   for (let playerGameTable of state.games) {
-    const gamePlayers = Object.keys(playerGameTable).filter((player, ind, arr) => players.indexOf(player) !== -1);
+    const gamePlayers = Object.keys(playerGameTable)
+                              .filter((player, ind, arr) => players.indexOf(player) !== -1);
     let maxValue = -1;
     for (let playerName of gamePlayers) {
       if (playerGameTable[playerName][9].totalScores > maxValue) {
