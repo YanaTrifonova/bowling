ef07d43a033c40d537a97b3f4d7ba89f00fe2117
YanaTrifonova <trifonova.yana.prof@gmail.com>
2021-09-20 02:26:18 +0200

    add action for score update in state

--- src/store/GameState/actions.js
+++ src/store/GameState/actions.js
@@ -1,5 +1,6 @@
 export const CREATE_NEW_GAME = "CREATE_NEW_GAME";
 export const RESET_ALL_GAMES = "RESET_ALL_GAMES";
+export const UPDATE_SCORES = "UPDATE_SCORES"
 
 export function createNewGame(data) {
   return {
@@ -12,5 +13,19 @@ export function resetAllGames() {
   return {
     type: RESET_ALL_GAMES,
     payload: null,
+  }
+}
+
+export function updateScores(score, oldScores, gameIndex, playerName, frameIndex, kickIndex) {
+  return {
+    type: UPDATE_SCORES,
+    payload: {
+      score: score,
+      oldScores: oldScores,
+      gameIndex: gameIndex,
+      playerName: playerName,
+      frameIndex: frameIndex,
+      kickIndex: kickIndex,
+    }
   }
 }
