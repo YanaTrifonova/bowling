bf84fa34343aab7a7243a7d5dadf428542a71887
YanaTrifonova <trifonova.yana.prof@gmail.com>
2021-09-20 02:31:49 +0200

    rewrite logic of kick updates to fix bugs, allow disabled frames and support computations in reducer

--- src/Components/GameFrame/index.jsx
+++ src/Components/GameFrame/index.jsx
@@ -2,28 +2,61 @@ import React, {useState} from 'react';
 import {MenuItem, Select} from "@material-ui/core";
 import {Typography} from "@mui/material";
 import PropTypes from "prop-types";
-import {initialSecondKickPossibleHit, kickPossibleHit} from "../../constants";
+import {useDispatch, useSelector} from "react-redux";
+import {updateScores} from "../../store/GameState/actions";
+import {getFrameScore} from "../../store/GameState/selector";
 
 export default function GameFrame({
                                     playerName,
-                                    gameNumber,
+                                    gameIndex,
                                     frameIndex,
-                                    hasThirdKick
+                                    hasThirdKick,
+                                    isDisabled,
                                   }) {
+  const initialSecondKickPossibleHit = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "/"];
+  const kickPossibleHit = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "X"];
   const totalFrames = 2 + (hasThirdKick ? 1 : 0);
   const [secondKickPossibleHit, setSecondKickPossibleHit] = useState(initialSecondKickPossibleHit);
   const [kicks, setKicks] = useState(Array(totalFrames).fill(""));
   const [isDisabledAfterTwo, setIsDisabledAfterTwo] = useState(true);
+  const [spareValue, setSpareValue] = useState(10);
+  const totalFrameScore = useSelector(getFrameScore(gameIndex, playerName, frameIndex));
+  const dispatch = useDispatch();
+
+  function toNumericScore(value) {
+    if (value === "X") {
+      return 10;
+    }
+    if (value === "/") {
+      return spareValue;
+    }
+    if (value === undefined) {
+      return 0;
+    }
+    return value;
+  }
+
+  function sumOldScores(index) {
+    return kicks.filter((val, ind, arr) => ind >= index)
+                .map((val, ind, arr) => toNumericScore(val))
+                .reduce((acc, val) => acc + val, 0);
+  }
 
   function onFirstKick(value, index) {
     const kicksUpdate = Array(totalFrames).fill("");
+    const oldScores = sumOldScores(index);
+    const numericScore = toNumericScore(value);
     kicksUpdate[index] = value;
     setKicks(kicksUpdate);
 
     let secondKickPossibleHit = []
     if (value !== "X") {
-      secondKickPossibleHit = initialSecondKickPossibleHit.slice(0, 9 - value);
-      secondKickPossibleHit.push("/")
+      secondKickPossibleHit = initialSecondKickPossibleHit.slice(0, 10 - value);
+      const updateSpareValue = secondKickPossibleHit[secondKickPossibleHit.length - 1];
+      if (updateSpareValue !== "/") {
+        setSpareValue(updateSpareValue);
+      }
+      secondKickPossibleHit[secondKickPossibleHit.length - 1] = "/";
       setIsDisabledAfterTwo(true);
     } else {
       setIsDisabledAfterTwo(false);
@@ -31,20 +64,23 @@ export default function GameFrame({
         secondKickPossibleHit = kickPossibleHit;
       }
     }
-
     setSecondKickPossibleHit(secondKickPossibleHit);
+    dispatch(updateScores(numericScore, oldScores, gameIndex, playerName, frameIndex, index));
   }
 
-  function onSecondAndLaterKick(value, index) {
+  function onSecondAndThirdKick(value, index) {
     if (value === "/" || value === "X") {
       setIsDisabledAfterTwo(false);
-    } else {
+    } else if (index !== 2) {
       setIsDisabledAfterTwo(true);
     }
     const emptyElements = Array(totalFrames - index - 1).fill("");
     const kicksUpdate = kicks.slice(0, index);
+    const oldScores = sumOldScores(index);
+    const numericScore = toNumericScore(value);
     kicksUpdate[index] = value;
     setKicks([...kicksUpdate, ...emptyElements]);
+    dispatch(updateScores(numericScore, oldScores, gameIndex, playerName, frameIndex, index));
   }
 
   function getSelectValue(index) {
@@ -62,28 +98,28 @@ export default function GameFrame({
       "borderBottom": "1px solid grey"
     }}>
       <div style={{"marginLeft": "20px"}}>
-        {
-          kicks.map((kickValue, index) => {
-            return (<Select labelId={"demo-simple-select-label" + index}
-                            key={playerName + "_" + gameNumber + "_" + frameIndex + "_" + index}
-                            id={"demo-simple-select" + index} value={kickValue}
-                            disabled={isDisabledAfterTwo && index > 1}
-                            onChange={(event) => {
-                              if (index === 0) {
-                                return onFirstKick(event.target.value, index);
-                              }
-                              return onSecondAndLaterKick(event.target.value, index)
-                            }}
-                            defaultValue={"-"}>
+        {kicks.map((kickValue, index) => {
+          return (
+            <Select labelId={"demo-simple-select-label" + index}
+                    key={playerName + "_" + gameIndex + "_" + frameIndex + "_" + index}
+                    id={"demo-simple-select" + index} value={kickValue}
+                    disabled={isDisabledAfterTwo && index > 1 || isDisabled}
+                    onChange={(event) => {
+                      if (index === 0) {
+                        return onFirstKick(event.target.value, index);
+                      }
+                      return onSecondAndThirdKick(event.target.value, index);
+                    }}
+                    defaultValue={"-"}>
               {getSelectValue(index).map((hit) => {
                 return (<MenuItem value={hit} key={hit}>{hit}</MenuItem>)
               })}
-            </Select>);
-          })
-        }
+            </Select>
+          );
+        })}
       </div>
       <div>
-        <Typography variant="body1" align="center" mt={2} mb={1}>0</Typography>
+        <Typography variant="body1" align="center" mt={2} mb={1} >{totalFrameScore}</Typography>
       </div>
     </div>
   )
@@ -91,7 +127,8 @@ export default function GameFrame({
 
 GameFrame.propTypes = {
   playerName: PropTypes.string,
-  gameNumber: PropTypes.number,
+  gameIndex: PropTypes.number,
   frameIndex: PropTypes.number,
   hasThirdKick: PropTypes.bool,
+  isDisabled: PropTypes.bool,
 }
--- src/store/GameState/selector.js
+++ src/store/GameState/selector.js
@@ -1,1 +1,2 @@
 export const getAllGames = state => state.games;
+export const getFrameScore = (gameIndex, playerName, frameIndex) => state => state.games[gameIndex - 1][playerName][frameIndex]?.totalScores;
